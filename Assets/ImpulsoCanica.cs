4337c7ed2fcf1ebdaaaef58c7ac9df9d207f2aa4
Armando Meza Leyva <armezaleyva@gmail.com>
2019-02-22 11:22:26 -0700

    Canicas y script de pruba inicial

---
+++ Assets/ImpulsoCanica.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulsoCanica : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(Vector2.right * 5, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
