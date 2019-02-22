6746e2590ab3f58b2d0829699787c9fea181fa63
Armando Meza Leyva <armezaleyva@gmail.com>
2019-02-22 11:26:37 -0700

    Serializefield para controlar la fuerza del impulso desde Unity

--- Assets/ImpulsoCanica.cs
+++ Assets/ImpulsoCanica.cs
@@ -6,11 +6,13 @@ public class ImpulsoCanica : MonoBehaviour
 {
 
     Rigidbody2D rigidbody;
+    [SerializeField]
+    int fuerzaDeImpulso = 5;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.AddForce(Vector2.right * 5, ForceMode2D.Impulse);
+        rigidbody.AddForce(Vector2.right * fuerzaDeImpulso, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
